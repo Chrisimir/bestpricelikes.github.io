576bcce0f811df48f6861c6f38ec963462959787
Chrisimir <chrislund97@gmail.com>
2019-12-14 21:29:23 +0100

    Add files via upload

---
+++ components/ElementHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center">
+  <b-jumbotron bg-variant="info" text-variant="white" fluid>
+    <template v-slot:header>Bootstrap Vue</template>
+
+    <template v-slot:lead>
+      This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+      featured content or information.
+    </template>
+
+  </b-jumbotron>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+    <JumboHero />
+
+    <b-card
+      img-src="https://placekitten.com/300/300"
+      img-alt="Card image"
+      title="Card Title"
+      img-right
+    >
+      <b-card-text>Some quick example text to build on the card and make up the bulk of the card's content.</b-card-text>
+    </b-card>
+
+    <ElementHeader />
+    <FeaturesCards />
+  </div>
+</template>
+
+<script>
+import JumboHero from "@/components/JumboHero";
+import ElementHeader from "@/components/ElementHeader";
+import FeaturesCards from "@/components/FeaturesCards";
+
+export default {
+  components: {
+    JumboHero,
+    ElementHeader,
+    FeaturesCards
+  }
+};
+</script>
+
+<style>
+</style>
